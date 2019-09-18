f54214dbc887bf97c063243b1f6fecbd9bc41080
newellp2019 <peter.newell@covetrus.com>
2019-09-18 15:17:28 -0400

    update

--- __init__.py
+++ __init__.py
@@ -2,15 +2,15 @@ from .handlers import setup_handlers
 # Jupyter Extension points
 def _jupyter_server_extension_paths():
     return [{
-        'module': 'trains-jupyter-plugin',
+        'module': 'jupytergitcommit',
     }]
 
 def _jupyter_nbextension_paths():
     return [{
         "section": "notebook",
-        "dest": "trains-jupyter-plugin",
+        "dest": "jupytergitcommit",
         "src": "static",
-        "require": "trains-jupyter-plugin/main"
+        "require": "jupytergitcommit/main"
     }]
 
 def load_jupyter_server_extension(nbapp):
