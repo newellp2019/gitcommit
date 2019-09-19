1cd9c6bde497e6be36c75b30fb8999df6cb00026
newellp2019 <peter.newell@covetrus.com>
2019-09-19 10:53:39 -0400

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="jupytergitcommit",
-    version='0.7',
+    version='0.8',
     url="https://github.com/newellp2019/jupytergitcommit",
     author="Peter Newell",
     description="Jupyter extension to enable user push notebooks to a git repo",
